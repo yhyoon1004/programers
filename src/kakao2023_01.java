8e2e97c8e3513d0636dae438d2dd731c11f07d3b
yunhwan <yhyoon1004@naver.com>
2023-07-20 23:21:13 +0900

    kakao2023_01 | SimpleDateFormat을 이용한 풀이 과정 중

--- src/kakao2023_01.java
+++ src/kakao2023_01.java
@@ -1,7 +1,9 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class kakao2023_01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Solution a = new Solution();
         String today = "2022.05.19";
         String[] terms = {"A 6", "B 12", "C 3"};
@@ -19,47 +21,35 @@ public class kakao2023_01 {
 }
 
 class Solution {
-    public int[] solution(String today, String[] terms, String[] privacies) {
+//    String today = "2022.05.19";
+//    String[] terms = {"A 6", "B 12", "C 3"};
+//    String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
+
+    public int[] solution(String today, String[] terms, String[] privacies) throws ParseException {
         List<Integer> result = new ArrayList<>();
-        String[] _today = today.split("[.]");
-        int[] i_today = new int[3];
-        i_today[0] = Integer.parseInt(_today[0]);
-        i_today[1] = Integer.parseInt(_today[1]);
-        i_today[2] = Integer.parseInt(_today[2]);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+        Date today_date = sdf.parse(today);
+        Map<String, Integer> termMap = new HashMap<>();
 
-        Map<String, Integer> _terms = new HashMap<>();
-
-        for (String v : terms) {
-            String[] temp = v.split("\\s");
-            _terms.put(temp[0], Integer.parseInt(temp[1]));
+        for (String term : terms) {
+            String[] term_value = term.split("\\s");
+            termMap.put(term_value[0], Integer.parseInt(term_value[1]));
         }
 
-        Integer[] date = new Integer[3];
+        int count =0;
+        for (String privacy : privacies) {
+            count++;
+            String[] privacy_value = privacy.split("\\s");
 
-        int counter = 1;
-        for (String v : privacies) {
-            String[] temp = v.split("\\s");
-            String[] dateTemp = temp[0].split("[.]");
-            date[0] = Integer.parseInt(dateTemp[0]);
-            date[1] = Integer.parseInt(dateTemp[1]);
-            date[2] = Integer.parseInt(dateTemp[2]);
-            date[1] = date[1] + _terms.get(temp[1]);
-            if (date[1] > 12) {
-                date[0] += (date[1] / 12);
-                date[1] -= (date[1] / 12) * 12;
+            Date parsedPrivacy_date = sdf.parse(privacy_value[0]);
+
+           Long privacyTime =  (termMap.get(privacy_value[1])*2592000000L) + parsedPrivacy_date.getTime();
+            if (today_date.getTime() > privacyTime) {
+                result.add(count);
             }
+        }
 
-            if (i_today[0] < date[0]) {
-                result.add(counter);
-            } else if (i_today[0] == date[0] && i_today[1] < date[1]) {
-                result.add(counter);
-            } else if (i_today[1] == date[1] && i_today[2] < date[2]) {
-                result.add(counter);
-            }
-            counter++;
-        }//end of for
-
-        return result.stream().mapToInt(i -> i).toArray();
+        return result.stream().mapToInt(i->i).toArray();
     }
 }
 
